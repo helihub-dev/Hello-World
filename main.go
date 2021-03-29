75d1576dc33c0bbe4aa8f4718c4bcb301b3ad8bd
helihub-dev <64596822+helihub-dev@users.noreply.github.com>
2021-03-29 16:20:37 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World")
+  fmt.Println("Heli's Hello World program using a programming language I never used before. Google Go.")
+}
